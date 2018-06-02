11921472f26cc12fb4764ac6a9f86c187fda82c4
shuangfanguang <695166116@qq.com>
2018-06-03 01:42:50 +0800

    搭建后台代码

---
+++ PhonemikeServer/PhonemikeServer.Host/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PhonemikeServer.WebApi;
+
+namespace PhonemikeServer.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            
+            host.Run();
+
+            while (true)
+            {
+                var inputStr = Console.ReadLine();
+
+                if (inputStr == "stop")
+                {
+                    host.Dispose();
+                    break;
+                }
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var host =    WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+            return host;
+        }
+    }
+}
