d9bd1ebc73ce266337db2b37cff5bb15db651f7d
shuangfanguang <695166116@qq.com>
2018-06-06 22:53:14 +0800

    tabs页不会被软键盘顶上去

--- PhonemikeClient/src/pages/tabs/tabs.ts
+++ PhonemikeClient/src/pages/tabs/tabs.ts
@@ -4,6 +4,8 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
+import { Keyboard } from 'ionic-angular'
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -13,7 +15,10 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(public keyboard: Keyboard) {
+    this.keyboard.onClose(()=>{
 
+    });
+    //this.keyboard.willShow();
   }
 }
